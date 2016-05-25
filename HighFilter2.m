00d89f0a75b24e47b90f0ef1e24f511f031fe84c
Yujie LU <donkey.3456@163.com>
2016-05-25 11:26:35 +0800

    Update HighFilter2.m

--- HighFilter2.m
+++ HighFilter2.m
@@ -1,4 +1,5 @@
 function filtered = HighFilter2(input,r)
+%This function is a high pass filter. It set the low-frequency spectrum with a radius of r to zero. Here the unit of r is pixel.
     [yn,xn] = size(input);
     [XX,YY] = meshgrid(1:xn,1:yn);
     Filter = (XX - xn/2 -1).^2 + (YY - yn/2 -1).^2 >= r^2;
