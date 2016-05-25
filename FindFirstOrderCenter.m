3a40adbd740b369e6f4dcbe244575e3d3e0d0ce8
Yujie LU <yjlu@mae.cuhk.edu.hk>
2016-05-25 12:48:36 +0800

    Add function FindFirstOrderCenter
    This function could find out the position of the first order center.

---
+++ FindFirstOrderCenter.m
@@ -0,0 +1,12 @@
+function [fy,fx] = FindFirstOrderCenter(im,HighpassR)
+    [yn,xn] = size(im);
+    [XX,YY] = meshgrid(1:xn,1:yn);
+    Filter = (XX - xn/2 -1).^2 + (YY - yn/2 -1).^2 > HighpassR^2;
+    fim = fft2(im);
+    fim = fftshift(fim);
+    fim = fim .* Filter;
+    absfim = abs(fim);
+    [v,u] = find(absfim == max(absfim(:)),1,'first');
+    fy = v - yn/2 - 1;
+    fx = u - xn/2 - 1;
+end
